df99ad6d7b7a46f298ffab87122766110fe48cbd
Panyafuang <panyafuang@gmail.com>
2020-04-30 21:06:08 +0700

    Updates

---
+++ precache-manifest.8bb1ed3bf94a0f74dfe3bd8f26c095f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dafe7c7dbff6fa52a7ba8e902f0de2c2",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "393a935de05733ee1a8d",
+    "url": "/monster-rolodex/static/css/main.2b883ccb.chunk.css"
+  },
+  {
+    "revision": "017031ba14a3fb92a040",
+    "url": "/monster-rolodex/static/js/2.772e5a82.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.772e5a82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "393a935de05733ee1a8d",
+    "url": "/monster-rolodex/static/js/main.b4d960e3.chunk.js"
+  },
+  {
+    "revision": "bc09d86fba44e1c7ba83",
+    "url": "/monster-rolodex/static/js/runtime-main.8000f3b5.js"
+  }
+]);
